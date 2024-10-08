d780a1029e0d189f8b77edb42db130162e609575
Manoranjanhere <manoranjanheremano@gmail.com>
2024-10-09 01:38:30 +0530

    done with work

---
+++ README.md
@@ -0,0 +1,1 @@
+I am Manoranjan Sahoo and here is my Portfolio Website.
---
+++ src/data/constants.js
@@ -0,0 +1,515 @@
+import apnaImage from './images/apna.png';
+import feedback from './images/feedback.png';
+import gocip from './images/gocip.png';
+import IEEE from './images/IEEE.png';
+import stewk from './images/Stweak.png';
+import portfolio from './images/portfolio.png';
+import lepses from './images/Lepses.png';
+import netflix from './images/netflix.png';
+import constitution from './images/constitution.png';
+import id from './images/id.png';
+import cer from './images/st.png';
+
+export const Bio = {
+  name: "Manoranjan Sahoo",
+  roles: [
+    "Full Stack Developer",
+    "DSA Programmer",
+    "Public Speaker",
+    "Sports Enthusiast",
+  ],
+  description:
+    "I am a quick learner and creative thinker with a passion for problem-solving. Eager to connect with industry experts, contribute innovative ideas, and adapt to new technologies in the web development field.",
+  github: "https://github.com/Manoranjanhere",
+  resume:
+    "https://drive.google.com/file/d/1Vxh2T8w1o1NLmO168v6FzLLozCZF3my9/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/manoranjansahoo1221/",
+  leetcode: "https://leetcode.com/u/Manoranjansahoo/",
+  feedback: "https://feedbacker-git-main-manoranjanheres-projects.vercel.app/u/Apnarooms_by_Mano",
+  Gmail: "mailto:manoranjanheremano@gmail.com/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Three.js",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/threejs/threejs-original.svg",
+      },
+      {
+        name: "Angular",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angular/angular-original.svg",
+      },
+      {
+        name: "jQuery",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg",
+      },
+      {
+        name: "Vite",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vite/vite-original.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg",
+          
+      },
+      {
+        name: "EJS",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg", // JavaScript logo as placeholder
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "TypeScript",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "PHP",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Programming languages",
+    skills: [
+      {
+        name: "C++",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postman/postman-original.svg",
+
+      },
+      {
+        name: "Jira",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jira/jira-original.svg",
+      },
+      {
+        name: "Figma",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+
+      },
+      {
+        name: "Vercel",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vercel/vercel-original.svg",
+      },
+      {
+        name: "DigitalOcean",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/digitalocean/digitalocean-original.svg",
+      },
+      {
+        name: "Linux",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://seeklogo.com/images/D/DevCo_Philippines-logo-CF15D3244B-seeklogo.com.gif",
+    role: "Cloud Engineer Intern",
+    company: "Devco services",
+    desc: "Working as a Cloud Engineer for company's customers.",
+    skills: [
+      "AWS",
+      "EC2",
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://app.outlier.ai/static/img/outlier/favicons/favicon-32x32.svg",
+    role: "LLM Trainer(Technical)",
+    company: "Outlier",
+    desc: "Training AI Model for Response",
+    skills: [
+      "JSON",
+      "Python",
+      "C++",
+      "Java",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://avatars.githubusercontent.com/u/140276445?s=200&v=4",
+    role: "Fullstack Devloper",
+    company: "IEEE LPU",
+    desc: "Full stack Devloper at research Organisation LPU.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Ejs",
+      "Mongoose",
+      "MongoDB",
+      "Node Js",
+      "Express Js",
+      "Bootstrap",
+      "Hamlet",
+      "Cookie Parser",
+      "Joi",
+      "Tailwind",
+      "JWT",
+      "Passport",
+      "flash",
+      "bcrypt",
+      "Next js",
+      "React js",
+      "Cloudinary",
+      "Digital Ocean",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    desc: "Contributing to different open-source projects",
+    doc: id,
+  },
+  {
+    id: 3,
+    img: "https://dim.mcusercontent.com/cs/cb032256fd43d7e96195b9381/images/54af6dce-c1d2-b11f-8ada-b066f51d8551.jpg?w=793&dpr=1",
+    role: "Co CEO and Devloper at Stewk AP and LEPSES",
+    company: "Stwek AP",
+    desc: "Devloper and Co CEO at Organisations.",
+    skills: [
+      "Leadership",
+      "Public Speaking",
+      "Web Devlopment",
+      "Figma",
+    ],
+    doc: cer,
+  },
+  {
+    id: 4,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Coursera-Logo_600x600.svg/2048px-Coursera-Logo_600x600.svg.png",
+    role: "Learner",
+    company: "Coursera",
+    desc: "Learned and grasped a lot of knowlege from the following cources.",
+    skills: [
+      "Next js",
+      "Mongoose",
+      "MongoDB",
+      "ZOD",
+      "Bootstrap",
+      "Typescript",
+      "Tailwind",
+      "Regexr",
+      "bcrypt",
+      "Next Auth",
+      "JWT",
+      "OTP",
+      "Resend",
+      "Open AI",
+      "React hook",
+      "Sadcn",
+      "debouncing",
+      "Axios",
+      "Drive link: 👇"
+    ],
+    doc:"https://drive.google.com/drive/folders/1BckfR_hvi0nNZV3pNQwcv8LcoI_bw5Ml?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://seekvectors.com/storage/images/Lovely%20Professional%20University-04.svg",
+    school: "Lovely Professional University, Phagwara",
+    date: "Sept 2022 - Sep 2026",
+    grade: "8.11 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Lovely Professional University, Punjab. I have completed 4 semesters and have a CGPA of 8.11. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.  I am learning and working on exciting projects.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://cf-vectorizer-live.s3.amazonaws.com/vectorized/2n76aAkiK5XfZx2vdAAyJk8lO9d/2n77wnHU33npr18UQbe92HAdtj6.svg",
+    school: "Ispat English Medium School, Rourkela",
+    date: "Apr 2020 - Apr 2022",
+    grade: "85%",
+    desc: "I completed my class 12 high school education at Ispat English Medium School, Rourkela",
+    degree: " ISC(XII) ",
+  },
+  {
+    id: 2,
+    img: "https://cf-vectorizer-live.s3.amazonaws.com/vectorized/2n76aAkiK5XfZx2vdAAyJk8lO9d/2n76wWz8p9WxJo0bmB5gnnXiA0d.svg",
+    school: "Chinmaya Vidyalaya English Medium School, Rourkela",
+    date: "Apr 2011 - Apr 2020",
+    grade: "93%",
+    desc: "I completed my class 10 education at Chinmaya Vidyalaya English Medium School, Rourkela",
+    degree: "ICSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Apna Rooms",
+    description:
+      "Developed a user-centric PG booking website for LPU students, utilizing JavaScript, HTML, CSS, andPassport to create secure logins, detailed listings, and integrated Google Maps.Successfully increased bookings by 40%, serving over 5,000 students with features like price comparisons,owner contacts, and student reviews.",
+    image: apnaImage,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Ejs",
+      "Mongoose",
+      "MongoDB",
+      "Node Js",
+      "Express Js",
+      "Bootstrap",
+      "Hamlet",
+      "Cookie Parser",
+      "Joi",
+      "Tailwind",
+      "JWT",
+      "Passport",
+      "flash",
+      "bcrypt",
+      "Cloudinary",
+      "Digital Ocean",
+    ],
+    category: "web app",
+    github: "https://github.com/Manoranjanhere/ApnaRooms",
+    webapp: "https://apnarooms.tech/listings",
+  },
+  {
+    id: 0,
+    title: "GOCIP",
+    description:
+      "Designed and implemented a full-fledged e-commerce platform for craft enthusiasts using React,Node.js, Tailwind, and MongoDB Atlas. Enhanced user experience with advanced filtering and secureJWT-based authentication.Integrated multiple payment options, including cash on delivery, and provided an admin dashboard fororder management, resulting in a 60% increase in sales and over 500 active customers.",
+    image: gocip,
+    tags: [
+      "React js",
+      "Mongoose",
+      "MongoDB",
+      "Node Js",
+      "Jsx",
+      "Bootstrap",
+      "Hamlet",
+      "Tailwind",
+      "JWT",
+      "toastify",
+      "bcrypt",
+      "axios",
+      "hooks",
+      "slug",
+      "express formidable",
+      "Braintree",
+      "Render",
+    ],
+    category: "web app",
+    github: "https://github.com/Manoranjanhere/Gocip_by_Mano",
+    webapp: "https://gocip-by-mano.onrender.com/",
+  },
+  {
+    id: 1,
+    title: "FeedBacker",
+    description:
+      "Developed an AI-powered feedback website with Next.js and OpenAI, enabling users to submit andreceive detailed project reviews. Utilized AI to analyze feedback and generate actionable insights.Facilitated diverse user participation, enhancing the feedback process with random inputs, making theplatform a valuable tool for refining projects.",
+    image: feedback,
+    tags: [
+      "Next js",
+      "Mongoose",
+      "MongoDB",
+      "ZOD",
+      "Bootstrap",
+      "Typescript",
+      "Tailwind",
+      "Regexr",
+      "bcrypt",
+      "Next Auth",
+      "JWT",
+      "OTP",
+      "Resend",
+      "Open AI",
+      "React hook",
+      "Sadcn",
+      "debouncing",
+      "Axios",
+      "Toaster",
+      "Vercel",
+    ],
+    category: "web app",
+    github: "https://github.com/Manoranjanhere/Feedbacker",
+    webapp: "https://feedbacker-git-main-manoranjanheres-projects.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "IEEE Website",
+    description:
+    "IEEE Research Organisation Lovely Professional University Information website which includes all projects past events and about the organisation",
+    image: IEEE,
+    tags: [
+      "Next js",
+      "Bootstrap",
+      "Typescript",
+      "Tailwind",
+      "React hook",
+      "Sadcn",
+      "Vercel",
+    ],
+    category: "web app",
+    github: "https://github.com/Manoranjanhere/IEEE_CIS_LPU/tree/main/public",
+    webapp: "https://ieeelpu.tech/",
+  },
+  {
+    id: 2,
+    title: "Stewk AP",
+    description:
+      "Landing page of Stewk AP Organisation, a landing page for organizational recognition.",
+    image:stewk,
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/Manoranjanhere/Stewk",
+    webapp: "http://stewkappvtltd.com/",
+  },
+  {
+    id: 10,
+    title: "My Portfolio Website",
+    description:
+      "Used React js for making an interactive portfolio website giving all information regarding me.",
+    image: portfolio,
+    tags: ["React js", "JavaScript", "Tailwind"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 4,
+    title: "Gamified Learning Platform for Constitutional Education",
+    description:
+      "develop an interactive and gamified digital platform that simplifies and demystifies the Indian Constitution, making it accessible and engaging for diverse audiences. The platform aims to enhance constitutional literacy, foster civic engagement, and empower users by transforming complex legal concepts into enjoyable learning experiences through games, a chatbot, and comprehensive resources.(will be live soon)",
+    image:constitution,
+    tags: ["React Js", "HTML", "CSS", "Node JS","Express","Ejs","Passport","JWT","ChatBot","OpenAI"],
+    category: "un app",
+    github: "https://github.com/Manoranjanhere/SIH24/",
+  },
+  {
+    id: 5,
+    title: "Lepses Landing page",
+    description:
+      " Landing page for Lepses Organisation, highlighting its mission, services, and impact. The page is designed to engage visitors, offering clear insights into the organization’s goals and how it drives innovation and community growth.This succinctly communicates the work done while emphasizing its purpose.",
+    image:lepses,
+    tags: ["HTML", "CSS", "JS"],
+    category: "un app",
+  },
+  {
+    id: 6,
+    title: "Netflix Clone",
+    description:
+      "I have created a simple Netflix clone, featuring basic functionality like  viewing content categories, seeing FAQ, and a responsive interface. The layout closely mirrors Netflix’s design, built using web technologies for the front end and styled-components for visual consistency.",
+    image:netflix,
+    tags: ["HTML", "CSS", "JS"],
+    category: "un app",
+  },
+];
